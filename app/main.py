f0a4f69c41bb1f5930344d0543e3662c8d3b152b
Enzo F Borges <enzo.alternativo@hotmail.com>
2025-03-21 09:01:42 -0300

    feat: listando palavras

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from exceptionPersonalizada import  ExtrapolacaoDeCaracteres
 
+
+
 texto = str(input("Digite o texto a ser verificado:"))
 try:
     if len(texto)>2048:
         raise ExtrapolacaoDeCaracteres("Texto com número de caracteres maior que o permitido!")
 except ExtrapolacaoDeCaracteres as e:
     print(e)
+    
+texto.split() # lista as palavras
